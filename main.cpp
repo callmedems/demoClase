d7919c1dded6caa61903975523464f3f5aa0ceb5
callmedems <[demmi.zepeda@gmail.com]>
2024-11-22 17:48:13 -0600

    inventory changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "inventory.h"
+#include "inventory.cpp"
 
 using namespace std;
 
 int main(){
+    Inventory inv;
+
+    string name = "audifonos";
+
+    inv.addProduct(name, 3);
+    inv.getProduct(name);
+    
 
     return 0;
 }
